5ba41c7fc763364b84f66655937bfdff551d7d93
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-04 10:32:41 +0200

    Layout added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span clasName="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
