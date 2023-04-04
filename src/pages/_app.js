6c0598ba17122a4eaa62b4736b916ec6d1845e7a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-04 10:17:55 +0200

    Header component added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import "tailwindcss/tailwind.css";
+
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
