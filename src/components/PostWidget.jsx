09849465be8cb41ebf8f313c05acff4efad2090d
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-05 23:00:27 +0200

    PostWidget update add

--- src/components/PostWidget.jsx
+++ src/components/PostWidget.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [slug]);
+  return;
 };
 
 export default PostWidget;
